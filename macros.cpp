0d49f2eb00886b49d1e3e558349ab97d160c9314
Akshit Thakur <100484073+akshit2257@users.noreply.github.com>
2024-02-19 05:45:10 +0530

    macros

---
+++ macros.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+#define pi 3.14
+
+int main(){
+    int r;
+    double area;
+    cout<<"Enter the radius"<<" ";
+    cin>>r;
+    area=pi*r*r;
+    cout<<"area is "<<area<<endl;
+    return 0;
+}
